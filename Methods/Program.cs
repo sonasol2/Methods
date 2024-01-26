63bfc43aeca6f0f1ec1d161161c665294c4bb5bd
sonasol2 <57984322+sonasol2@users.noreply.github.com>
2024-01-26 17:04:19 +0200

    method remember

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -3,7 +3,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        void SayHello()
+        {
+            Console.WriteLine("Hello");
+	    }
+
+        void PrintMsg(string message)
+        {
+            Console.WriteLine(message);
+	    }
+
+        PrintMsg("Hello");
+        PrintMsg("Yo");
+
+        void Summ(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine(result); 
+	    }
+
+        Summ(13, 4);
+
+
+        void PrintPerson(string name, int age = 0, string company = "undefined")
+        {
+            Console.WriteLine($"Имя: {name}, возраст: {age},компания: {company}.");
+	    }
+
+        PrintPerson("Bob", 24);
+        PrintPerson("Fred", 32, "Amazon");
+
+
     }
 }
 
